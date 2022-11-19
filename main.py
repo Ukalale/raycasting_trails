d1a57239fd196aa125cb3a1d8ff46cc225577cc0
Ukalale <lucaguiga@gmail.com>
2022-11-19 01:15:18 -0800

    Entry 2
    These are the same beginnings however with the additive of graphics, and mouse movements.
    
    Note that mouse movement is continuous in its range, however the domain seems to jump out of the app and cease working.

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from settings import *  # import from settings tab in the whole doomgame folder
 from map import *
 from player import *
 from raycasting import *
-
+from object_renderer import *
 
 class Game:
     def __init__(self):
         pg.init()  # pygame modules
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)  # render the set resolution in an instance of a clock class
         self.clock = pg.time.Clock()
         self.delta_time = 1 # the time passed since the last frame
@@ -17,6 +18,7 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
 
 
@@ -28,9 +30,10 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')  # display the current FPS
 
     def draw(self):
-        self.screen.fill('black')  # paint the screen black
-        #self.map.draw()
-        #self.player.draw()
+        # self.screen.fill('black')  # paint the screen black
+        self.object_renderer.draw()
+        # self.map.draw()
+        # self.player.draw()
 
     def check_events(self):  # method/loop to check closing the tab with escape key and if said events occur, exiting the application
         for event in pg.event.get():
